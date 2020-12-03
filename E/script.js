e99a247b6336c45f080be53510e5ff29909e1bef
190107062@stu.sdu.edu.kz <190107062@stu.sdu.edu.kz>
2020-12-03 14:52:02 +0600

    3 tasks are done

--- E/script.js
+++ E/script.js
@@ -1,3 +1,11 @@
-
-
 /* Write your code here */
+
+let elements = tasks.querySelectorAll('div');
+for(let i = 0; i<elements.length; i++) {
+	let butt = elements[i].querySelector('button');
+	butt.addEventListener('click', fun);
+
+	function fun() {
+		elements[i].dataset.status = 'done';
+	}
+}
--- M/script2.js
+++ M/script2.js
@@ -1,5 +1,30 @@
 let countries = ["Kazakhstan","Russia","England","France"];
 let cities_by_country = {"Kazakhstan":["Almaty","Astana","Karagandy"],"Russia":["Moscow","Saint-Petersburg","Kazan"],"England":["London","Manchester","Liverpool"],"France":["Paris","Lyon","Marseille"]};
-
 /* Write your code here */
+
+let countries1 = document.querySelector('#countries');
+let cities1 = document.querySelector('#cities');
+
+for(let i = 0; i<=countries.length; i++) {
+	options = document.createElement('option');
+	options.textContent = countries[i];
+	countries1.add(options);
+}
+
+countries1.addEventListener("change", fun)
+
+function fun() {
+	let remov = cities1.querySelectorAll("option");
+	for(let i = 0; i < remov.length; i++) {
+		remov[i].remove();
+	}
+	for(let j = 0; j < cities_by_country[this.value].length; j++) {
+		let city_option = document.createElement("option");
+		city_option.textContent = cities_by_country[this.value][j];
+		cities1.add(city_option);
+	}
+}
+
+
+
 
